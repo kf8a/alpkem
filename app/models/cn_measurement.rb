b321be65f9b2acf0d6571d753c64b92d67ffdbd0
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-10-11 09:40:57 -0400

    Removes find_all_good_measurements methods

--- app/models/cn_measurement.rb
+++ app/models/cn_measurement.rb
@@ -1,15 +1,12 @@
 class CnMeasurement < ActiveRecord::Base
+
+  belongs_to :analyte
   belongs_to :cn_sample
   belongs_to :run
-  belongs_to :analyte
   
+  validates_presence_of :amount
   validates_presence_of :analyte
   validates_presence_of :cn_sample
-  validates_presence_of :amount
-  
-  def CnMeasurement.find_all_good_measurements()
-    CnMeasurement.find(:all, :conditions => ['deleted = false'])
-  end
   
   def sample
     return cn_sample
