3a6c6909f69381a2a42ff014f767f4a856b61bbf
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-10-11 12:10:37 -0400

    Upgrades session_store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alpkem::Application.config.secret_token = 'cf174fa0abdfb8f2924fe7a4b3224ce90c18443c0e26a8d5bfcd403ca4ac783051ebaa512f431a9bac5c79c983cd9aa36c1c996ad458e9622ed5c1e93a54e479'
