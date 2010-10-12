aaa1e1f082b3981d7e1f90c2ee10be604112d2ad
Jonathon D. Jones <jonathon_d_jones@yahoo.com>
2010-10-11 22:12:55 -0400

    Adds my seeds back in

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,28 @@
-# l_study = Study.find_or_create_by_name(:name => 'Lux Arbor', :prefix => 'M')
-# 
-# 1.upto(3) do |i|
-#   Treatment.find_or_create_by_name(:name => "L0#{i}", :study_id => l_study.id)
-# end
-# 
-# 1.upto(10) do |i|
-#   Replicate.find_or_create_by_name(:name => "S#{i}", :study_id => l_study.id)
-# end
-# 
-# l_study.treatments.each do |treatment|
-#   replicates = Replicate.find_all_by_study_id(l_study)
-#   replicates.each do |replicate|
-#     p = Plot.find_or_create_by_name(:name => "#{treatment.name}#{replicate.name}", :study => l_study, :treatment => treatment, :replicate => replicate).save
-#   end
-# end
-
-l_study = Study.find_or_create_by_name(:name => 'Main Site', :prefix => 'T')
+ l_study = Study.find_or_create_by_name(:name => 'Lux Arbor', :prefix => 'M')
+ 
+ 1.upto(3) do |i|
+   Treatment.find_or_create_by_name(:name => "L0#{i}", :study_id => l_study.id)
+ end
+ 
+ 1.upto(10) do |i|
+   Replicate.find_or_create_by_name(:name => "S#{i}", :study_id => l_study.id)
+ end
+ 
+ l_study.treatments.each do |treatment|
+   replicates = Replicate.find_all_by_study_id(l_study)
+   replicates.each do |replicate|
+     p = Plot.find_or_create_by_name(:name => "#{treatment.name}#{replicate.name}", :study => l_study, :treatment => treatment, :replicate => replicate).save
+   end
+ end
 
 
-l_study.treatments.each do |treatment|
-  replicates = Replicate.find_all_by_study_id(l_study)
+m_study = Study.find_or_create_by_name(:name => 'Main Site', :prefix => 'T')
+
+m_study.treatments.each do |treatment|
+  replicates = Replicate.find_all_by_study_id(m_study)
   replicates.each do |replicate|
     1.upto(3) do |f|
-      p = Plot.find_or_create_by_name(:name => "#{treatment.name}#{replicate.name}F#{f}", :study => l_study, :treatment => treatment, :replicate => replicate).save
+      p = Plot.find_or_create_by_name(:name => "#{treatment.name}#{replicate.name}F#{f}", :study => m_study, :treatment => treatment, :replicate => replicate).save
     end
   end
 end
