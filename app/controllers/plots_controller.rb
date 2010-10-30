5133704ffb0bf6ed3f0a63361b93882589a8b789
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-10-30 18:03:22 -0400

    Moves much of create_plots logic to study model

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -16,22 +16,10 @@ class PlotsController < ApplicationController
     study = Study.find_or_create_by_name(:name => params[:study_name], :prefix => params[:prefix])
     
     number_of_treatments = params[:number_of_treatments].to_i
-    1.upto(number_of_treatments) do |i|
-      Treatment.find_or_create_by_name_and_study_id(:name => "#{study.prefix}#{i}", :study_id => study.id)
-    end
-
     replicate_prefix = params[:replicate_prefix]
     number_of_replicates = params[:number_of_replicates].to_i
-    1.upto(number_of_replicates) do |i|
-      Replicate.find_or_create_by_name_and_study_id(:name => "#{replicate_prefix}#{i}", :study_id => study.id)
-    end
 
-    study.treatments.each do |treatment|
-      replicates = Replicate.find_all_by_study_id(study)
-      replicates.each do |replicate|
-        p = Plot.find_or_create_by_name_and_study_id(:name => "#{treatment.name}#{replicate.name}", :study => study, :treatment => treatment, :replicate => replicate)
-      end
-    end
+    study.create_plots(number_of_treatments, number_of_replicates, replicate_prefix)
     redirect_to :action => 'show', :id => study.id
   end
 
--- app/models/study.rb
+++ app/models/study.rb
@@ -4,4 +4,21 @@ class Study < ActiveRecord::Base
   has_many :plots
   
   validates_uniqueness_of :name
+
+  def create_plots(number_of_treatments, number_of_replicates, replicate_prefix)
+    1.upto(number_of_treatments) do |i|
+      Treatment.find_or_create_by_name_and_study_id(:name => "#{self.prefix}#{i}", :study_id => self.id)
+    end
+
+    1.upto(number_of_replicates) do |i|
+      Replicate.find_or_create_by_name_and_study_id(:name => "#{replicate_prefix}#{i}", :study_id => self.id)
+    end
+
+    self.treatments.each do |treatment|
+      replicates = Replicate.find_all_by_study_id(self)
+      replicates.each do |replicate|
+        Plot.find_or_create_by_name_and_study_id(:name => "#{treatment.name}#{replicate.name}", :study => self, :treatment => treatment, :replicate => replicate)
+      end
+    end
+  end
 end
