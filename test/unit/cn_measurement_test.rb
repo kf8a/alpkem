0876f6264448aa2382b97ee7906ada2b69fd6c5f
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-11-02 16:57:02 -0400

    CN measurement model is now fully tested

--- test/unit/cn_measurement_test.rb
+++ test/unit/cn_measurement_test.rb
@@ -1,5 +1,15 @@
 require 'test_helper'
 
 class CnMeasurementTest < ActiveSupport::TestCase
- 
+
+  context "a sample with a cn measurement" do
+    setup do
+      @sample = Factory.create(:cn_sample)
+      @measurement = Factory.create(:cn_measurement, :cn_sample => @sample)
+    end
+
+    should "return its cn_sample when asked for sample" do
+      assert @measurement.sample == @sample
+    end
+  end
 end
