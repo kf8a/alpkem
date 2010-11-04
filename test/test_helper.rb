e57e92902ef95dfab6b8558718547bdb60a665e8
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-11-04 10:01:20 -0400

    No reason to keep commented out simplecov code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,3 @@
-# coding: UTF-8
-if RUBY_VERSION > "1.9"
-  # require 'simplecov'
-  # SimpleCov.start 'rails'
-end
-
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
