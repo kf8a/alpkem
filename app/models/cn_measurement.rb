221d0a9d9f265ba8db8614fbc04f35a030d106fa
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-11-08 12:03:53 -0500

    Creates some helper functions to ease transition to measurement

--- app/models/cn_measurement.rb
+++ app/models/cn_measurement.rb
@@ -7,7 +7,17 @@ class CnMeasurement < ActiveRecord::Base
   validates :amount,    :presence => true
   validates :analyte,   :presence => true
   validates :cn_sample, :presence => true
-  
+
+  def self.real_plots
+    self.all.collect {|measurement| measurement.cn_plot if measurement.plot_is_real?}
+  end
+
+  def plot_is_real?
+    !self.cn_plot.blank? &&
+        !self.cn_plot.include?("Standard") &&
+        !self.cn_plot.include?("Blindstd")
+  end
+
   def sample
     return cn_sample
   end
