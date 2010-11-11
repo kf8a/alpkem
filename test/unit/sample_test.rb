94145fc44d2bc5998653ffa570728c04d7a9ef8c
Jonathon Jones <jonathon_d_jones@yahoo.com>
2010-11-10 23:33:44 -0500

    Converts cn_measurement_test and sample_test to minitest

--- test/unit/sample_test.rb
+++ test/unit/sample_test.rb
@@ -1,22 +1,27 @@
 require 'test_helper'
+require 'minitest/autorun'
 
-class SampleTest < ActiveSupport::TestCase
+describe Sample do
 
-  should validate_presence_of :plot
+  it "should validate presence of plot" do
+    @sample = Factory.build(:sample)
+    @sample.plot = nil
+    refute @sample.valid?
+  end
 
-  context "plot_name method" do
-    setup do
+  describe "plot_name method" do
+    before do
       @sample = Factory.create(:sample)
     end
 
-    should "return the name of the plot" do
+    it "should return the name of the plot" do
       assert @sample.plot_name == @sample.plot.name
     end
   end
   
-  context "previous_measurements method" do
-    context "with some previous approved measurements for the same plot" do
-      setup do
+  describe "previous_measurements method" do
+    describe "with some previous approved measurements for the same plot" do
+      before do
         @sample = Factory.create(:sample, :sample_date => Date.today)
         @prev_sample = Factory.create(:sample,
             :sample_date => 1.year.ago.to_date,
@@ -29,7 +34,7 @@ class SampleTest < ActiveSupport::TestCase
              :plot => @sample.plot,
              :approved => false)
         @prev_unapproved = Factory.create(:measurement, :sample => @unapproved_sample)
-        new_plot = Factory.create(:plot, :name => "Different")
+        new_plot = Factory.create(:plot)
         @wrong_plot_sample = Factory.create(:sample,
              :sample_date => 1.year.ago.to_date,
              :plot => new_plot,
@@ -38,43 +43,43 @@ class SampleTest < ActiveSupport::TestCase
         @prev_deleted = Factory.create(:measurement, :sample => @prev_sample, :deleted => true)
       end
 
-      should "list those previous measurements" do
+      it "should list those previous measurements" do
         assert @sample.previous_measurements.include?(@prev_approved1)
         assert @sample.previous_measurements.include?(@prev_approved2)
       end
 
-      should "not list unapproved measurements" do
+      it "should not list unapproved measurements" do
         assert !@sample.previous_measurements.include?(@prev_unapproved)
       end
 
-      should "not list measurements from other plots" do
+      it "should not list measurements from other plots" do
         assert !@sample.previous_measurements.include?(@prev_wrong_plot)
       end
 
-      should "not list measurements which were deleted" do
+      it "should not list measurements which were deleted" do
         assert !@sample.previous_measurements.include?(@prev_deleted)
       end
     end
   end
 
-  context "analytes method" do
-    setup do
+  describe "analytes method" do
+    before do
       @sample = Factory.create(:sample)
     end
     
-    should "include NO3" do
+    it "should include NO3" do
       no3 = Analyte.find_by_name("NO3")
       assert @sample.analytes.include?(no3)
     end
 
-    should "include NH4" do
+    it "should include NH4" do
       nh4 = Analyte.find_by_name("NH4")
       assert @sample.analytes.include?(nh4)
     end
   end
 
-  context "average method" do
-    setup do
+  describe "average method" do
+    before do
       @sample = Factory.create(:sample)
       @no3 = Analyte.find_by_name("NO3")
       @nh4 = Analyte.find_by_name("NH4")
@@ -87,7 +92,7 @@ class SampleTest < ActiveSupport::TestCase
       @sample.reload
     end
 
-    should "return the average only of the right measurements" do
+    it "should return the average only of the right measurements" do
       assert_equal @sample.average(@no3), @correct_average
       assert_equal @sample.average(@nh4), 4
     end
