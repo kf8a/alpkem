a8fb8d2ebbf536ebcbbf69d9a4099d00ec3b8650
Jonathon Jones <jonathon_d_jones@yahoo.com>
2011-02-02 12:35:38 -0500

    Greatly speeds up file_parser_test

--- test/unit/parsers/file_parser_test.rb
+++ test/unit/parsers/file_parser_test.rb
@@ -1,20 +1,21 @@
 require File.expand_path('../../../test_helper',__FILE__) 
+require 'minitest/autorun'
 
-class FileParserTest < ActiveSupport::TestCase
-  
-    def setup  
-      @parser = FileParser.for(9, Date.today)
-    end
-    
-    def test_that_the_same_sample_is_returned
-      @parser.find_plot('T1R1')
-      @parser.create_sample
-      sample = @parser.sample
-      @parser.find_sample
-      assert_equal sample, @parser.sample
-      @parser.find_or_create_sample
-      assert_equal sample, @parser.sample
-    end
+describe FileParser do
+
+  before do
+    @parser = FileParser.for(9, Date.today)
+  end
+
+  it "should return the right sample on find_sample and find_or_create_sample" do
+    @parser.find_plot('T1R1')
+    @parser.create_sample
+    sample = @parser.sample
+    @parser.find_sample
+    assert_equal sample, @parser.sample
+    @parser.find_or_create_sample
+    assert_equal sample, @parser.sample
+  end
  
 end
 
