dc99381f3029a41e7934a0f305472b0a41e0f78d
Sven Bohm <bohms@msu.edu>
2011-02-17 11:45:39 -0500

    updated with new regex

--- app/models/parsers/cn_deep_parser.rb
+++ app/models/parsers/cn_deep_parser.rb
@@ -1,15 +1,17 @@
 #For parsing CN Deep Core files into Carbon/Nitrogen measurements.
 class CNDeepParser < CNSampleParser
 
-  CN_DEEP_CORE        = ',\d*,\d*(.{1,11})[abc|ABC]?,(\d*\.\d*),\w*,(\w*),\w*,\w*,\w*,(\d*\.\d*),(\d*\.\d*)'
+#  CN_DEEP_CORE        = ',\d*,\d*(.{1,11})[abc|ABC]?,(\d*\.\d*),\w*,(\w*),\w*,\w*,\w*,(\d*\.\d*),(\d*\.\d*)'
+  CN_DEEP_CORE = '(\d+),\d+,"\d*(DC.{1,11})[ABC]",.+,(\d+\.\d+),(\d+\.\d+)'
 
   def process_line(line)
     re = Regexp.new(CN_DEEP_CORE)
 
     if line =~ re
-      @plot_name   = $1
-      @percent_n   = $4
-      @percent_c   = $5
+      @sample_date = $1
+      @plot_name   = $2
+      @percent_n   = $3
+      @percent_c   = $4
 
       process_data
     end
