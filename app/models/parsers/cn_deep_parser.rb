d531c3504a3b57a73572339aa8a72eac13d7a7b9
Sven Bohm <bohms@msu.edu>
2011-02-18 10:07:09 -0500

    added excel csv parsing

--- app/models/parsers/cn_deep_parser.rb
+++ app/models/parsers/cn_deep_parser.rb
@@ -2,7 +2,7 @@
 class CNDeepParser < CNSampleParser
 
 #  CN_DEEP_CORE        = ',\d*,\d*(.{1,11})[abc|ABC]?,(\d*\.\d*),\w*,(\w*),\w*,\w*,\w*,(\d*\.\d*),(\d*\.\d*)'
-  CN_DEEP_CORE = '(\d+),\d+,"\d*DC(.{1,11})[ABC]",.+,(\d+\.\d+),(\d+\.\d+)'
+  CN_DEEP_CORE = '(\d+),\d+,"?\d*DC(.{1,11})[ABC]"?,.+,(\d+\.\d+),(\d+\.\d+)'
 
   def process_line(line)
     re = Regexp.new(CN_DEEP_CORE)
