390e1cef651bafe4a5ae09c458503e461b45c7c3
Sven Bohm <bohms@msu.edu>
2011-10-21 11:53:41 -0400

    added database_authenticable temporarily to work around a bug in openid_authenticable https://github.com/nbudin/devise_openid_authenticatable/issues/11

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :openid_authenticatable, :rememberable
+  devise :openid_authenticatable, :rememberable, :database_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :remember_me
+
+  #HACK to make the newer devise work with openid_authenticatable
+  attr :encrypted_password
 end
