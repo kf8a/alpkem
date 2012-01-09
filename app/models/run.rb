32dc83109d8b0c7d4cbb47bab74a42a756ed4320
Sven Bohm <bohms@msu.edu>
2012-01-09 16:16:36 -0500

    order the compounds

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,7 +3,7 @@ class Run < ActiveRecord::Base
   belongs_to :sample_type
   has_many :measurements, :dependent => :destroy
   has_many :samples, :through => :measurements, :uniq => true
-  has_many :analytes, :through => :measurements, :uniq => true
+ has_many :analytes, :through => :measurements, :uniq => true, :order => 'name'
 
   validates :sample_type_id, :presence => true
   validates :measurements,   :presence => true
