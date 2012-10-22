43e8ca12b46068d58310d56608c3c0931f34be06
Sven Bohm <bohms@msu.edu>
2012-10-22 09:44:09 -0400

    change small negative numbers to zero

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,5 +1,6 @@
 #Represents a single measurement, e.g. an amount of carbon or nitric acid.
 class Measurement < ActiveRecord::Base
+  before_save :change_negative_to_zero
 
   belongs_to :analyte
   belongs_to :sample
@@ -11,5 +12,13 @@ class Measurement < ActiveRecord::Base
 
   def sample_date
     self.sample.sample_date
+  end
+
+  # change number to zero unless they are more that 0.01 negative
+  # in which case we need to investigate.
+  def change_negative_to_zero
+    if amount < 0 && amount > -0.01
+      amount = 0
+    end
   end
 end
