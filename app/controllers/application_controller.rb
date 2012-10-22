2c7b8d319bc2037bc90ea5c37317c5d212be94be
Sven Bohm <bohms@msu.edu>
2012-10-22 11:43:02 -0400

    skip auth token on json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 #Methods that apply to all controllers
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  skip_before_filter :verify_authenticity_token, :if => :json_request?
 
   before_filter :authenticate_user!, :except => [:index, :show] unless Rails.env == 'test'
 
