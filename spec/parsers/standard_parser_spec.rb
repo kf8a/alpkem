5d579ebd7c4e7edacc5d80777cc7024a8f3a7e4d
Sven Bohm <bohms@msu.edu>
2012-11-13 11:41:58 -0500

    Test for the standard parser

--- spec/parsers/standard_parser_spec.rb
+++ spec/parsers/standard_parser_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe Parsers::StandardParser do
-  it 'parses double element files' do
-    file = File.read('./test/data/100419M.TXT')
+    before do
+      @parser = Parsers::FileParser.for(2,Date.today)
+    end
+    describe 'a line of data' do
+      before do
+        @parser.process_line('10:38	101	2-1a	     6282	   0.048					    74108	   0.714				')
+      end
 
+      it "should have the right plot" do
+        @parser.sample.plot.name.should == 'T2R1'
+      end
+      it "should have the right measurement" do
+        assert_includes @parser.measurements.collect {|x| x.amount}, 0.048
+        assert_includes @parser.measurements.collect {|x| x.amount}, 0.714
+      end
   end
 end
