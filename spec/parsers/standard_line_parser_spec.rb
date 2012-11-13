ad2f92e5a77bbd68e7b501c39e82dab9f957fa5f
Sven Bohm <bohms@msu.edu>
2012-11-13 14:38:44 -0500

    added spec for standard_line_parser

--- spec/parsers/standard_line_parser_spec.rb
+++ spec/parsers/standard_line_parser_spec.rb
@@ -3,9 +3,9 @@ require './app/models/parsers/standard_line_parser.rb'
 describe StandardLineParser do
   it 'parses soil sample lines' do
     first, second, nh4, no3 = StandardLineParser.parse('10:38	101	2-1a	     6282	   0.048					    74108	   0.714				')
-    first.should == '2'
-    second.should == '1'
-    nh4.should == '0.048'
-    no3.should == '0.714'
+    first.should    == '2'
+    second.should   == '1'
+    nh4.should      == '0.048'
+    no3.should      == '0.714'
   end
 end
