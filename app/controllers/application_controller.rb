7497ebd4ee3754ac1f41e2226f646cfd3c588700
Sven Bohm <bohms@msu.edu>
2012-11-14 10:09:36 -0500

    openid issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 #Methods that apply to all controllers
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery :except => :create
 
   before_filter :authenticate_user!, :except => [:index, :show] unless Rails.env == 'test'
 
