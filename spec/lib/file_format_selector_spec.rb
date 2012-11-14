21720815ad34ca5fbfc15d69cb648fae05fc23df
Sven Bohm <bohms@msu.edu>
2012-11-14 09:09:39 -0500

    added specs for FileFormatSelector

---
+++ spec/lib/file_format_selector_spec.rb
@@ -0,0 +1,16 @@
+require './lib/file_format_selector.rb'
+
+describe FileFormatSelector do
+  it 'finds the right file line parser prefix for NO3 alpkem files' do
+      file = File.open('test/data/3262012B.TXT')
+      FileFormatSelector.new.get_line_parser_prefix(file).should == 'NO3'
+  end
+  it 'finds the right file line parser prefix for NO3 alpkem files' do
+      file = File.open('test/data/3262012B-nh4.TXT')
+      FileFormatSelector.new.get_line_parser_prefix(file).should == 'NH4'
+  end
+  it 'finds the right line parser prefix for dual elment alpkem files' do
+    file = File.open('test/data/100419L.TXT')
+    FileFormatSelector.new.get_line_parser_prefix(file).should == ''
+  end
+end
