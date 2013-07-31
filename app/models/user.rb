aa7cd0683e876d02f3408507b43d1e0ed1b9e1d8
Sven Bohm <bohms@msu.edu>
2013-07-31 11:09:32 -0400

    removed openid line

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :openid_authenticatable, :rememberable, :database_authenticatable
+  devise :rememberable, :database_authenticatable, :recoverable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :remember_me
+  attr_accessible :remember_me, :email, :password
 
   #HACK to make the newer devise work with openid_authenticatable
   attr :encrypted_password
