7014333552315934fb0ac6df3a1679012776feac
Sven Bohm <bohms@msu.edu>
2013-11-05 08:12:36 -0500

    use swf prefix for switchgrass plots

---
+++ db/seed-swf-plots.rb
@@ -0,0 +1,8 @@
+swf_glbrc_study = Study.find_or_create_by_name(:name => 'SWF', :prefix => 'SWF')
+
+1.upto(8) do |treatment|
+  [100,200,300,400] do |block|
+      plot = treatment + block
+      Plot.where(:name => "SWF#{plot}", :study_id => swf_glbrc_study).first_or_create({:name => "SWF#{plot}}#{depth}", :study => swf_glbrc_study})
+  end
+end
--- spec/parsers/swf_parser_spec.rb
+++ spec/parsers/swf_parser_spec.rb
@@ -4,13 +4,13 @@ describe Parsers::SWFParser do
 
   describe 'a line of data' do
     before do
-      FactoryGirl.create :plot, name: '404'
+      FactoryGirl.create :plot, name: 'SWF404'
       @parser = Parsers::FileParser.for(18,Date.today)
       @parser.process_line('20111101,15,SWF5404H1B,,2.822,GLBRC11P16B3,Unknown,,,,0.7014,45.972')
     end
 
     it 'returns the right plot' do
-      @parser.plot.name.should == '404'
+      @parser.plot.name.should == 'SWF404'
     end
 
     it 'returns the right C value' do
