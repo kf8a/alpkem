a43363556ebc6203c2c226cdac53f1d977bdc77a
Sven Bohm <bohms@msu.edu>
2013-11-05 14:48:04 -0500

    try strings again

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,18 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Alpkem::Application.config.secret_token = SecureRandom.hex(64)
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+Alpkem::Application.config.secret_token = secure_token
