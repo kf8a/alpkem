f298439bd54e56df5d81f80323b425e506dadfe9
Sven Bohm <bohms@msu.edu>
2014-01-09 20:18:54 -0500

    silence deprecation warning

--- config/application.rb
+++ config/application.rb
@@ -54,6 +54,8 @@ module Alpkem
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+      I18n.enforce_available_locales = true
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
