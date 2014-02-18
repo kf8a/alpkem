02d6f92d44686df3a9e47dc12b776749bf812462
Sven Bohm <bohms@msu.edu>
2014-02-18 16:22:48 -0500

    Revert "remove swf parser"
    This reverts commit dba06492e1541ee0469c083b1ce073361aaca931.

---
+++ app/models/parsers/swf_parser.rb
@@ -0,0 +1,15 @@
+class Parsers::SWFParser < Parsers::CNSampleParser
+
+  SWF_CN = '(\d+),\d+,"?(SWF)(\d)(\d{3})(H\d)[ABC]"?,.+,(\d+\.\d+),(\d+\.\d+)'
+
+  def process_line(line)
+    raw_date, @prefix, @rate, @plot_name, @harvest, @percent_n, @percent_c = ParserMatcher.parse(SWF_CN, line)
+
+    if cn_plot_name_ok?
+      @plot_name = @prefix + @rate + @plot_name + @harvest
+      @sample_date = Date.parse(raw_date) if raw_date
+      process_data 
+    end
+  end
+
+end
