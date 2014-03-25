c63af792b1bdd51ae78e550a18d6b1db2d3ddec2
Sven Bohm <bohms@msu.edu>
2014-03-25 13:40:26 -0400

    make a,b,c optional for now and remove old parser

--- app/parsers/generic_line_parser.rb
+++ app/parsers/generic_line_parser.rb
@@ -1,6 +1,6 @@
 class GenericLineParser
 
-  GENERIC_SAMPLE     = '\t(\d{8})(\w+)-?(\d+)?-[abc|ABC]\s*(?: rerun)*\t\s+-*\d+\s+(-*\d+\.\d+)\t.*\t *-*\d+\t\s*(-*\d+\.\d+)'
+  GENERIC_SAMPLE     = '\t(\d{8})(\w+)-?(\d+)?-?[abc|ABC]?\s*(?: rerun)*\t\s+-*\d+\s+(-*\d+\.\d+)\t.*\t *-*\d+\t\s*(-*\d+\.\d+)'
 
   def self.parse(line)
     ParserMatcher.parse(GENERIC_SAMPLE, line)
