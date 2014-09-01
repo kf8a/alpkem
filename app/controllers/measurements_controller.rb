223ed7063f11db97db0fe63b85df492573956012
Sven Bohm <bohms@msu.edu>
2014-09-01 16:00:27 -0400

    whitespace edits

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,15 +1,16 @@
 #Methods to manipulate pages related to the Measurements model
 class MeasurementsController < ApplicationController
-  
+
   def destroy
     @run = Run.find(params[:run_id])
     @measurement = Measurement.find(params[:id])
     @measurement.toggle!(:deleted)
     @dom_id = "sample-#{@measurement.sample_id}-#{@measurement.analyte_id}"
-    
+
     @measurements = @run.measurements
     respond_to do |format|
       format.js
     end
   end
+
 end
