66112809999f90a8e080e890a7b01c942d654b35
Sven Bohm <bohms@msu.edu>
2014-08-31 20:40:33 -0400

    first step in upgrading to rails 4.1

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,11 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Alpkem
   class Application < Rails::Application
     config.autoload_paths << Rails.root.join('lib')
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
