ce64cad3925bc315304241dd9cb72df124f297b2
Sven Bohm <bohms@msu.edu>
2014-10-14 19:49:12 -0400

    also allow minus signs on nh4 data

--- lib/parsers/old_lysimeter_line_parser.rb
+++ lib/parsers/old_lysimeter_line_parser.rb
@@ -1,7 +1,7 @@
 module Parsers
   class OldLysimeterLineParser
 
-    LYSIMETER = '(\w{1,2})-(\d)-(\d)[ABC|abc], (\d{8})\s*\d+\.\d+\s+(-?\d+\.\d+).+\d+\.\d+\s+(-?\d+\.\d+)'
+    LYSIMETER = '(\w{1,2})-(\d)-(\d)[ABC|abc], (\d{8}).+\d+\.\d+\s+(-?\d+\.\d+).+\d+\.\d+\s+(-?\d+\.\d+)'
 
     def self.parse(line)
       first, second, third, raw_date, nh4_amount, no3_amount = ParserMatcher.parse(LYSIMETER, line)
