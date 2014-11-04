d629b82ffe689fec00888947e964f7976129a561
Sven Bohm <bohms@msu.edu>
2014-11-04 09:37:10 -0500

    use warn log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
   config.logger = RemoteSyslogLogger.new('logs2.papertrailapp.com', 34385)
+  config.log_level = :warn 
+
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
