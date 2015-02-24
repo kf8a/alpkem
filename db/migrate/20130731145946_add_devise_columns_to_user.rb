380d5fd65175d808a9939c7f7fc4c60953ae48be
Sven Bohm <bohms@msu.edu>
2015-02-24 18:22:51 -0500

    fix migration order

--- db/migrate/20130731145946_add_devise_columns_to_user.rb
+++ db/migrate/20130731145946_add_devise_columns_to_user.rb
@@ -1,7 +1,5 @@
 class AddDeviseColumnsToUser < ActiveRecord::Migration
   def change
-    add_column :users, :email, :string,  :null  => false, :default => ''
-    add_column :users, :encrypted_password, :string, :null => false, :default =>  ''
     add_column :users, :reset_password_token, :string
     add_column :users, :reset_password_sent_at, :datetime
     add_column :users, :sign_in_count, :integer, :default => 0
