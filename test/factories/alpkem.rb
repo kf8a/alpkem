7129477adceb6ebf05eadb48642ed4f584a538db
Sven Bohm <bohms@msu.edu>
2015-02-24 16:38:05 -0500

    simplify factory by removing block variables

--- test/factories/alpkem.rb
+++ test/factories/alpkem.rb
@@ -5,39 +5,39 @@ FactoryGirl.define do
   sequence :plot_name do |n|
     "plot#{n}#{rand(n).to_i}#{rand(4)}#{Time.now}"
   end
-  factory :analyte do |a|
-    a.name                 { FactoryGirl.generate :analyte_name }
+  factory :analyte do
+    name                 { FactoryGirl.generate :analyte_name }
   end
-  factory :plot do |p|
-    p.name                 { FactoryGirl.generate :plot_name }
+  factory :plot do
+    name                 { FactoryGirl.generate :plot_name }
   end
-  factory :replicate do |r|
+  factory :replicate do
   end
-  factory :study do |s|
+  factory :study do
   end
-  factory :treatment do |t|
+  factory :treatment do
   end
-  factory :user do |u|
+  factory :user do
   end
-  factory :sample do |s|
-    s.association :plot
+  factory :sample do
+    association :plot
   end
-  factory :measurement do |m|
-    m.association     :sample
-    m.association     :analyte
-    m.amount          0.5
+  factory :measurement do
+    association     :sample
+    association     :analyte
+    amount          0.5
   end
-  factory :run do  |r|
-    r.sample_type_id    1
-    r.measurements      [FactoryGirl.create(:measurement)]
+  factory :run do
+    sample_type_id    1
+    measurements      [FactoryGirl.create(:measurement)]
   end
-  factory :cnm, :class => :measurement do |m|
-    m.association   :sample
-    m.association   :analyte, :name => "N"
-    m.amount        0.5
+  factory :cnm, :class => :measurement do
+    association   :sample
+    association   :analyte, :name => "N"
+    amount        0.5
   end
-  factory :cn_run, :class => :run do |r|
-    r.sample_type_id    6
-    r.measurements   [FactoryGirl.create(:cnm)]
+  factory :cn_run, :class => :run do
+    sample_type_id    6
+    measurements   [FactoryGirl.create(:cnm)]
   end
 end
