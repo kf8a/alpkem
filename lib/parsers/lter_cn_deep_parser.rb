a5e01da2af8fd26f6c7e1163841e3d19829f3c97
Sven Bohm <bohms@msu.edu>
2015-08-05 15:05:44 -0400

    Revert "updated to new deep core soil format"
    This reverts commit e303e23f7bd58533694c1795b18fc2706933d371.

--- lib/parsers/lter_cn_deep_parser.rb
+++ lib/parsers/lter_cn_deep_parser.rb
@@ -1,18 +1,11 @@
 module Parsers
   class LterCnDeepParser < CNSampleParser
 
-    # REGEX = '(\d+),\d+,(T..(?:nt)?R\dS\dC\d[SMD])[ABC],.+,(\d+\.\d+),(\d+\.\d+)'
-    CN_DEEP_CORE_SAMPLE        = '(\d+),\d+,(T.+R\dS\dC\d)-(\d+)-[abc|ABC],\d+\.\d+,\w+,\w+,,,,(\d+\.\d+),(\d+\.\d+)'
+    REGEX = '(\d+),\d+,(T..(?:nt)?R\dS\dC\d[SMD])[ABC],.+,(\d+\.\d+),(\d+\.\d+)'
 
     def process_line(line)
-      raw_date, @plot_name, depth, @percent_n, @percent_c = ParserMatcher.parse(CN_DEEP_CORE_SAMPLE, line)
-      @sample_date = Date.parse(raw_date)
-      @plot_name = @plot_name + case depth.to_i
-      when 122 then "D"
-      when 55  then "M"
-      when 25  then "T"
-      when 10  then "S"
-      end
+      raw_date, @plot_name, @percent_n, @percent_c = ParserMatcher.parse(REGEX, line)
+      @sample_date = Date.parse(raw_date) if raw_date
       process_data if cn_plot_name_ok?
     end
   end
--- spec/lib/parsers/lter_cn_deep_parser_spec.rb
+++ spec/lib/parsers/lter_cn_deep_parser_spec.rb
@@ -4,35 +4,39 @@ describe Parsers::LterCnDeepParser do
 
   describe 'lter deep core data' do
     before do
-      FactoryGirl.create :plot, name: "T04R5S4C2M"
+      FactoryGirl.create :plot, :name => 'T06R3S3C2M'
       @parser = Parsers::Parser.for(15, Date.today)
-      @parser.process_line("20130904,10,T04R5S4C2-55-C,17.247,LTERDC2013P02A10,Unknown,,,,0.0159,0.1342")
-      # @parser.process_line("20100825,48,T06R3S3C2MB,17.12,LTERDC1D12,Unknown,,,,0.0261,0.1986")
+      @parser.process_line("20100825,48,T06R3S3C2MB,17.12,LTERDC1D12,Unknown,,,,0.0261,0.1986")
     end
 
     it 'has the right date' do
-      assert_equal Date.new(2013,9,4), @parser.sample.sample_date
+      assert_equal Date.new(2010,8,25), @parser.sample.sample_date
     end
 
     it 'has the right plot' do
-      assert_equal 'T04R5S4C2M', @parser.sample.plot.name
+      assert_equal 'T06R3S3C2M', @parser.sample.plot.name
     end
 
     it 'has the right right measurement' do
-      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.0159
-      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.1342
+      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.0261
+      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.1986
     end
   end
 
   describe 'a T8nt plot' do
     before do
-      FactoryGirl.create :plot, :name => "T08ntR5S4C2S"
+      FactoryGirl.create :plot, :name => "T08ntR3S2C1S"
       @parser = Parsers::Parser.for(15, Date.today)
-      @parser.process_line("20130904,10,T08ntR5S4C2-10-C,17.247,LTERDC2013P02A10,Unknown,,,,0.0159,0.1342")
+      @parser.process_line('20101029,21,T08ntR3S2C1SA,16.67,LTERDC20B9,Unknown,,,,0.0248,0.1526')
     end
 
     it 'has the right plot' do
-      assert_equal 'T08ntR5S4C2S', @parser.sample.plot.name
+      assert_equal 'T08ntR3S2C1S', @parser.sample.plot.name
+    end
+
+    it 'has the right measurements' do
+      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.0248
+      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.1526
     end
   end
 
@@ -40,12 +44,16 @@ describe Parsers::LterCnDeepParser do
     before do
       FactoryGirl.create :plot, :name=>'TSFR1S5C2M'
       @parser = Parsers::Parser.for(15, Date.today)
-      @parser.process_line("20130904,10,TSFR1S5C2-55-C,17.247,LTERDC2013P02A10,Unknown,,,,0.0159,0.1342")
+      @parser.process_line('20101027,82,TSFR1S5C2MA,18.779,LTERDC20G10,Unknown,,,,0.0045,0.0431')
     end
 
     it 'has the right plot' do
       assert_equal 'TSFR1S5C2M', @parser.sample.plot.name
     end
 
+    it 'has the right measurements' do
+      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.0045
+      assert_includes @parser.sample.measurements.collect {|x| x.amount}, 0.0431
+    end
   end
 end
