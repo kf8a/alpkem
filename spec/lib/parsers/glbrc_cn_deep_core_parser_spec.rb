0a872431039d1b3fb3f4d7832f93eaa0943e8315
Sven Bohm <bohms@msu.edu>
2015-10-06 15:03:27 -0400

    allow scaleup deepcores to go into the same place as the intensive site

--- spec/lib/parsers/glbrc_cn_deep_core_parser_spec.rb
+++ spec/lib/parsers/glbrc_cn_deep_core_parser_spec.rb
@@ -39,6 +39,22 @@ describe Parsers::GLBRCCNDeepCoreParser do
       assert_includes @parser.measurements.collect {|x| x.amount}, 0.2617
     end
   end
+  describe 'a line of scaleup data without the date in the sample field' do
+    before do
+      @parser.process_line('20131114,15,M01R1S1-50-B,16.982,B3GLBRC13DCP01,Unknown,,,,0.0368,0.2617')
+    end
+
+    it "should have the right date" do
+      expect(@parser.sample.sample_date).to eq(Date.civil(2013,11,14))
+    end
+    it "should have the right plot" do
+      expect(@parser.sample.plot.name).to eq('M1R1S1-50')
+    end
+    it "should have the right measurement" do
+      assert_includes @parser.measurements.collect {|x| x.amount}, 0.0368
+      assert_includes @parser.measurements.collect {|x| x.amount}, 0.2617
+    end
+  end
   describe 'a line of non data' do
     before do
       @parser.process_line 'Material for Analysis:  plants,,,Calibration Standard: Acetanilide,,Plate Label: GLBRC12 P04,,,,,'
