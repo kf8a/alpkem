b7ea4d8808b43987db89b525ab4f15a9871d51d7
Sven Bohm <bohms@msu.edu>
2016-01-08 15:43:28 -0500

    use with exception on protect from forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 #Methods that apply to all controllers
 class ApplicationController < ActionController::Base
-  protect_from_forgery :except => :create
+  # protect_from_forgery :except => :create
+  protect_from_forgery with: :exception
 
   before_filter :authenticate_user!, :except => [:index, :show] unless Rails.env == 'test'
 
