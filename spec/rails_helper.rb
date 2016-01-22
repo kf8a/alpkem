6ec468c5ef0be0ef4a5636eceef5b9e263a5189a
Sven Bohm <bohms@msu.edu>
2016-01-22 13:28:41 -0500

    use transactions to clean the database between runs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -40,9 +40,16 @@ RSpec.configure do |config|
 #   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
    config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # The different available types are documented in the features, such as in
-  # https://relishapp.com/rspec/rspec-rails/docs
-  # config.infer_spec_type_from_file_location!
+   config.before(:suite) do
+     DatabaseCleaner.strategy = :transaction
+     # DatabaseCleaner.clean_with(:truncation)
+   end
+
+   config.around(:each) do |example|
+     DatabaseCleaner.cleaning do
+       example.run
+     end
+   end
  end
 
 
