1a33346568a4e6a9129c7fd461de1aae8be738f1
Sven Bohm <bohms@msu.edu>
2016-02-04 15:04:12 -0500

    set up mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,17 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.active_record.raise_in_transactional_callbacks = true
+
+  config.action_mailer.delivery_method = :sendmail
+  Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i -t'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'no-reply@sebewa.kbs.msu.edu'}
+  config.action_mailer.default_url_options = { :host => 'sebewa.kbs.msu.edu' }
+
 end
+
