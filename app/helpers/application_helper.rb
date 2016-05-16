f9316804483d3c4115ea413842694b6678b572c2
Sven Bohm <bohms@msu.edu>
2016-05-16 08:54:53 -0400

    remove unused files and style fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def showing_progress(opts={})
-    opts.merge :loading  => update_page{|p| p[:spinner].show },
-               :complete => update_page{|p| p[:spinner].hide }
+  def showing_progress(opts = {})
+    opts.merge loading: update_page { |p| p[:spinner].show },
+               complete: update_page { |p| p[:spinner].hide }
   end
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,6 @@
+# This is supposed to represent a sampling station
+# but I don't think it is used anywhere
+# TODO: check and remove
 class Station < ActiveRecord::Base
   has_many :plots
-  # attr_accessible :title, :body
 end
