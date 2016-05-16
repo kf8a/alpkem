201bf0121f8e775b4943d6c446c896ab40f62ef0
Sven Bohm <bohms@msu.edu>
2016-05-16 08:50:45 -0400

    style fixes and removed old google chart helper

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -1,25 +1,9 @@
 require 'statistics'
 
+# Helper to display measurements
 module RunsHelper
-
-  def self.google_chart_script_helper(measurements, analytes)
-    analyte1 = analytes[0]
-    analyte2 = analytes[1]
-    
-    chart_script = setup_columns(analyte1, analyte2)
-    chart_script += add_measurements(measurements, analyte1)
-    chart_script.slice!(-1) if chart_script.end_with?(",")
-    chart_script += "]);"
-  end
-
-  def self.setup_columns(analyte1, analyte2)
-    "data.addColumn('date', 'Date');" +
-    "data.addColumn('number', '#{analyte1.name}');" +
-    "data.addColumn('number', '#{analyte2.name}');"
-  end
-
   def self.add_measurements(measurements, analyte1)
-    chart_measurements = "data.addRows(["
+    chart_measurements = 'data.addRows(['
     measurements.each do |measurement|
       chart_measurements += add_measurement(measurement, analyte1)
     end
@@ -42,10 +26,10 @@ module RunsHelper
   end
 
   def self.average(measurements)
-    Statistics.mean(measurements.map {|x| x.amount})
+    Statistics.mean(measurements.map(&:amount))
   end
 
   def self.cv_help(measurements)
-    Statistics.cv(measurements.map {|x| x.amount})
+    Statistics.cv(measurements.map(&:amount))
   end
 end
