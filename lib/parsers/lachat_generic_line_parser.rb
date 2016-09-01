3ed0977e66eb8d043298e4fbc566398f0f409360
Sven Bohm <bohms@msu.edu>
2016-09-01 14:39:14 -0400

    Revert "moved the columns for the lachat parser from 17 and 30 to 25 and 38"
    This reverts commit 1555d8a11e1b0e08f08882c34f32020b6eed155b.

--- lib/parsers/lachat_generic_line_parser.rb
+++ lib/parsers/lachat_generic_line_parser.rb
@@ -20,8 +20,8 @@ module Parsers
       depth = result[5]
       plot = first + second + third
 
-      nh4 = data[25].to_f
-      no3 = data[38].to_f
+      nh4 = data[17].to_f
+      no3 = data[30].to_f
       sample_date = Date.new(raw_date[0..3].to_i, raw_date[4..5].to_i, raw_date[6..7].to_i) if raw_date
 
       [sample_date, plot, depth, nh4, no3]
--- lib/parsers/lachat_standard_line_parser.rb
+++ lib/parsers/lachat_standard_line_parser.rb
@@ -23,8 +23,8 @@ module Parsers
       manual_dilution_factor = data[5].to_f
       auto_dilution_factor = data[6].to_f
       dilution_factor = manual_dilution_factor * auto_dilution_factor
-      nh4 = data[25].to_f #* dilution_factor
-      no3 = data[38].to_f #* dilution_factor
+      nh4 = data[17].to_f * dilution_factor
+      no3 = data[30].to_f * dilution_factor
       sample_date = Date.new(raw_date[0..3].to_i, raw_date[4..5].to_i, raw_date[6..7].to_i) if raw_date
 
       [first, second, nh4, no3, sample_date, modifier, site]
