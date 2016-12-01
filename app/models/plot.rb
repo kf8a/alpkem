95f86485ac571a5b89ea3e0df0c364c0664c22c6
Sven Bohm <bohms@msu.edu>
2016-11-30 21:02:51 -0500

    speling

--- app/models/plot.rb
+++ app/models/plot.rb
@@ -9,7 +9,7 @@ class Plot < ActiveRecord::Base
   validates :name, uniqueness: { scope: :study_id }
 
   def self.find_by_treatment_and_replicate(treatment_string, replicate_string)
-    trt = Treatment.find_by_(name: treatment_string)
+    trt = Treatment.find_by(name: treatment_string)
     rep = Replicate.find_by(name: replicate_string)
     Plot.find_by(treatment_id: trt.id, replicate_id: rep.id)
   end
