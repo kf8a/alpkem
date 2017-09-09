02ca535315d0e4f1b6364cbfada768d8ad8f07ab
Sven Bohm <bohms@msu.edu>
2017-09-09 15:25:26 -0400

    add description so that start and stop tasks show up with cap -T

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
-require "bundler/capistrano"
+require 'bundler/capistrano'
 load 'deploy/assets'
 
-set :application, "alpkem"
-set :repository,  "/Users/bohms/code/alpkem"
+set :application, 'alpkem'
+set :repository,  '/Users/bohms/code/alpkem'
 set :scm, :git
 
 # If you aren't deploying to /u/apps/#{application} on the target
@@ -14,37 +14,39 @@ set :deploy_to, "/var/u/apps/#{application}"
 set :user, 'deploy'
 set :use_sudo, false
 
-set :branch, "master"
+set :branch, 'master'
 set :deploy_via, :copy
-set :git_enable_submodules,1
+set :git_enable_submodules, 1
 
-role :app, 'sebewa.kbs.msu.edu'
-role :web, 'sebewa.kbs.msu.edu'
-role :db,  'sebewa.kbs.msu.edu', :primary => true
+role :app, 'oshtemo.kbs.msu.edu'
+role :web, 'oshtemo.kbs.msu.edu'
+role :db,  'oshtemo.kbs.msu.edu', primary: true
 
-set :unicorn_binary, "/usr/local/bin/unicorn"
-set :unicorn_config, "/etc/unicorn/alpkem"
+set :unicorn_binary, 'unicorn'
+set :unicorn_config, "#{current_path}/config/unicorn/production.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 namespace :deploy do
-  task :start, :roles => :app, :except => { :no_release => true } do 
+  desc 'start unicorn server'
+  task :start, roles: :app, except: { no_release: true } do
     run "cd #{current_path} && #{try_sudo} bundle exec #{unicorn_binary} -c #{unicorn_config} -E #{rails_env} -D"
   end
-  task :stop, :roles => :app, :except => { :no_release => true } do 
+  desc 'stop unicorn server'
+  task :stop, roles: :app, except: { no_release: true } do
     run "#{try_sudo} kill `cat #{unicorn_pid}`"
   end
-  task :graceful_stop, :roles => :app, :except => { :no_release => true } do
+  task :graceful_stop, roles: :app, except: { no_release: true } do
     run "#{try_sudo} kill -s QUIT `cat #{unicorn_pid}`"
   end
-  task :reload, :roles => :app, :except => { :no_release => true } do
+  task :reload, roles: :app, except: { no_release: true } do
     run "#{try_sudo} kill -s USR2 `cat #{unicorn_pid}`"
   end
-  task :restart, :roles => :app, :except => { :no_release => true } do
+  task :restart, roles: :app, except: { no_release: true } do
     stop
     start
   end
 
- # after :deploy, :link_paperclip_storage, 
+  after :deploy, :link_paperclip_storage
   after 'deploy:finalize_update', :link_production_db
   after 'deploy:finalize_update', :link_local_env
   after 'deploy:finalize_update', :link_upload_dir
@@ -52,18 +54,18 @@ namespace :deploy do
 end
 
 # seed database
-desc "seed the database"
+desc 'seed the database'
 task :seed_database do
   run "cd #{current_path}; RAILS_ENV=production bundle exec rake db:seed"
 end
 
 # database.yml task
-desc "Link in the production database.yml"
+desc 'Link in the production database.yml'
 task :link_production_db do
   run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
 end
 # local env
-desc "Link in the local_env.yml"
+desc 'Link in the local_env.yml'
 task :link_local_env do
   run "ln -nfs #{deploy_to}/shared/config/local_env.yml #{release_path}/config/local_env.yml"
 end
