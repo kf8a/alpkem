8956f8ff8538a587b8ced87d81029ce628f322d2
Sven Bohm <bohms@msu.edu>
2017-10-20 14:49:27 -0400

    removed extra print statement

--- lib/parsers/lysimeter_line_parser.rb
+++ lib/parsers/lysimeter_line_parser.rb
@@ -1,7 +1,7 @@
 module Parsers
   class LysimeterLineParser
 
-    LYSIMETER = '(\w{1,2})-(\d)-(\d)[ABC|abc], (\d{8})\s+-?\d+\t\s+(-?\d+\.\d+)\t\w*\t+\s+-?\d+\t\s+(-?\d+\.\d+)'
+    LYSIMETER = '(\w{1,2})-(\d)-(\d)[ABC|abc], (\d{8})\s+-?\d+\t\s+(-?\d+\.\d+)\t\w*\t+\s+-?\d+\t\s+(-?\d+\.\d+)'.freeze
 
     def self.parse(line)
       first, second, third, raw_date, nh4_amount, no3_amount = ParserMatcher.parse(LYSIMETER, line)
