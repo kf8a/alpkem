d247cbb6299c0830228d5567d1992760d2a0a116
Sven Bohm <bohms@msu.edu>
2019-03-06 12:54:40 -0500

    style fix

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Represents a single measurement, e.g. an amount of carbon or nitric acid.
 class Measurement < ActiveRecord::Base
   before_save :change_negative_to_zero
@@ -15,7 +17,8 @@ class Measurement < ActiveRecord::Base
   # change number to zero unless they are more that 0.01 negative
   # in which case we need to investigate.
   def change_negative_to_zero
-    return unless amount < 0 && amount > -0.01
+    return unless amount.negative? && amount > -0.01
+
     self.amount = 0
   end
 end
