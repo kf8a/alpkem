3a651d132b1b8ced8211f98948555cb3bd964726
Sven Bohm <bohms@msu.edu>
2019-05-03 08:41:09 -0400

    style fix

--- app/lib/parsers.rb
+++ app/lib/parsers.rb
@@ -80,7 +80,7 @@ module Parsers
               when 38 then Parsers::LysimeterParser # Rainout shelters
               when 39 then Parsers::GLBRCCNPitParser # MLE pit samples
               when 40 then Parsers::GLBRCCNRootSoilParser # GLBRC Root Soil samples
-              when 42 then Parsers::GLBRCCNPitRootParser# GLBRC Root pit samples
+              when 42 then Parsers::GLBRCCNPitRootParser # GLBRC Root pit samples
               when 43 then Parsers::GLBRCCNRootExcavationSoilParser # GLBRC root excavation soil pit samples
               when 44 then Parsers::GLBRCCNRootExcavationPlantParser # GLBRC root excavation soil pit samples
               else false
