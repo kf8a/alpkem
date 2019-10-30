53637a67cdf3b22d3d3f07c8cf68ec32db9cb918
sven <bohms@ideomatic.io>
2019-10-30 10:51:14 -0400

    set deploy location

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.2"
+lock '~> 3.11.2'
 
 set :application, 'alpkem'
 set :repo_url, 'https://github.com/kf8a/alpkem.git'
@@ -8,7 +8,7 @@ set :repo_url, 'https://github.com/kf8a/alpkem.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, '/var/u/app/alpkem'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
