28b0509e5c5a632d6a13de98c0252e5013a95f37
Sven Bohm <bohms@hey.com>
2022-08-17 12:24:44 -0400

    added deep core capablity to switchgras samples

--- app/lib/parsers/glbrc_switchgrass_cn_parser.rb
+++ app/lib/parsers/glbrc_switchgrass_cn_parser.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 module Parsers
+  # parser for switchgrass soil samples and deep core samples
   class GLBRCSwitchgrassCNParser < CNSampleParser
-
     SWITCHGRASS =
-      '(\d+),\d+,(SWF\d+H\d+)[abc|ABC],\d+\.\d+,\w+,\w+,,,,(\d+(?:\.\d+)?),(\d+(?:\.\d+)?)'
+      '(\d+),\d+,\d{2}?((DC)?SWF\d+H\d+)[abc|ABC],\d+\.\d+,\w+,\w+,,,,(\d+(?:\.\d+)?),(\d+(?:\.\d+)?)'
 
     def process_line(data)
-      raw_date, @plot_name, @percent_n, @percent_c = ParserMatcher.parse(SWITCHGRASS, data)
+      raw_date, @plot_name, _, @percent_n, @percent_c = ParserMatcher.parse(SWITCHGRASS, data)
       @sample_date = Date.parse(raw_date) if raw_date
 
       process_data
