7a6e1eb305e3d8f7759ce9e4985c8796622191eb
Sven Bohm <bohms@hey.com>
2023-03-21 10:36:54 -0400

    style fixes

--- app/lib/parsers/leilei_sample_parser.rb
+++ app/lib/parsers/leilei_sample_parser.rb
@@ -2,7 +2,7 @@ module Parsers
   # parsing leilei's samples
   class LeileiSampleParser < FileParser
 
-    LEILEI_SAMPLE = '\t\d{3}\t(\w\d+\w\d*-\d)\s+(\d+/\d+/\d+)\s+\d+\s+(-*\d\.\d+)\s+\d+\s+(-*\d+\.\d+)'
+    LEILEI_SAMPLE = '\t\d{3}\t(\w\d+\w\d*-\d)\s+(\d+/\d+/\d+)\s+\d+\s+(-*\d\.\d+)\s+\d+\s+(-*\d+\.\d+)'.freeze
 
     def process_line(line)
       re = Regexp.new(LEILEI_SAMPLE)
@@ -15,10 +15,10 @@ module Parsers
         date = $2
 
         self.sample_date = Chronic.parse(date).to_date
-        #try to find plot
+        # try to find plot
         self.plot = Plot.find_by(name: plot)
-        #create it if you can't find it
-        self.plot = Plot.create(:name => plot) if self.plot.blank?
+        # create it if you can't find it
+        self.plot = Plot.create(name: plot) if self.plot.blank?
 
         process_nhno_sample(nh4_amount, no3_amount)
       end
