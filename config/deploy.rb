a8468760f4b4c992c00da41b9af2f8b851000adb
Sven Bohm <bohms@hey.com>
2023-10-17 19:44:27 -0400

    add legacy ssl options

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,10 @@ set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets vendor/bundle public/sys
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, {
+  'NODE_OPTIONS':'--openssl-legacy-provider'
+}
+
 
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
