215db2a96160d9e253ad69743b1edce9e6de34dc
Sven Bohm <bohms@hey.com>
2023-10-17 19:49:13 -0400

    reduce the number of kept deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ set :default_env, {
 # set :local_user, -> { `git config user.name`.chomp }
 
 # Default value for keep_releases is 5
-set :keep_releases, 50
+set :keep_releases, 20
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
