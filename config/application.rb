2543962788d7d4bc1ef5e692fb6d10ba5a5d94b0
Sven Bohm <bohms@hey.com>
2023-10-17 20:36:02 -0400

    go to new application defaults

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,10 @@ module Alpkem
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Initialize configuration defaults for originally generated Rails version.
+    # config.load_defaults 5.2
+    config.load_defaults 6.0
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
