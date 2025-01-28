a63b0eecf802dcf3ad1134aeb9b2fa8f97e7864a
Sven Bohm <bohms@hey.com>
2025-01-28 09:09:46 -0500

    do not tie the regex to the line ending since windowns uses \r\n

--- app/lib/parsers/resource_gradient_cn_parser.rb
+++ app/lib/parsers/resource_gradient_cn_parser.rb
@@ -4,7 +4,7 @@ module Parsers
   # For parsing GLBRC surface soil Carbon and Nitrogen samples.
   class ResourceGradientCnParser< CNSampleParser
     CN_SOIL_SAMPLE =
-      '(\d+).(\d+).(\d+),\d+,(\d+-F\d)[A|B|C],.+,(\d+\.\d+),(\d+\.\d+)$'
+      '(\d+).(\d+).(\d+),\d+,(\d+-F\d)[A|B|C],.+,(\d+\.\d+),(\d+\.\d+)'
 
     def process_line(line)
       month, day, year, @plot_name, @percent_n, @percent_c = ParserMatcher.parse(CN_SOIL_SAMPLE, line)
@@ -14,7 +14,7 @@ module Parsers
       @sample_date = Date.new(year.to_i, month.to_i,day.to_i)
 
       @plot_name = @plot_name + '-' + '025'
-      _, treatment, _depth = @plot_name.split('-')
+      _plot, treatment, _depth = @plot_name.split('-')
 
       Plot.find_or_create_by(name: @plot_name, study_id: 10, top_depth: 0, bottom_depth: 25, treatment_name: treatment )
 
