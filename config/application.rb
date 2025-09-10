b940680dcd046851b4200f9854e03f62d2bb0a96
Sven Bohm <bohms@hey.com>
2025-09-10 09:08:56 -0400

    go to rails 7 defaults

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Alpkem
 
     # Initialize configuration defaults for originally generated Rails version.
     # config.load_defaults 5.2
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
