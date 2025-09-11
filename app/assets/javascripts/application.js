44590738c33547443bf2c7fe86a0922a10e501d7
Sven Bohm <bohms@hey.com>
2025-09-11 19:41:53 -0400

    remove the extra assets/javascript

--- app/assets/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-// Add new JavaScript/Coffee code in separate files in this directory and they'll automatically
-// be included in the compiled file accessible from http://example.com/assets/application.js
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-//= require jquery
-//= require rails-ujs
-//= require_tree .
-//
