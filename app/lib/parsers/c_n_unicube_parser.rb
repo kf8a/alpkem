e86a8557b38cfafc560f9d7a4bd3e78d3afb8668
Sven Bohm <bohms@hey.com>
2026-05-13 09:45:43 -0400

    update unicube to handle LTAR, LTER and  BCSE

--- app/lib/parsers/c_n_unicube_parser.rb
+++ app/lib/parsers/c_n_unicube_parser.rb
@@ -3,21 +3,22 @@
 module Parsers
   class CNUnicubeParser < CNSampleParser
     def process_line(line)
+      Logger.new(STDOUT).info("Processing line: #{line}")
       _weight, name, method, _area_n, _area_c, _factor_n, _factor_c, @percent_n, @percent_c, _rest = CSV.parse_line(line)
       # return unless the method is either plant or soil
-      return unless method.include?("Plant")
-      return if name == "Name"
+      return unless method.include?('Plant')
+      return if name == 'Name'
       return unless name.include?('-')
 
       # sometimes we have 3 parts other times we have 4 parts as in the strip samples
       result = name.split('-')
       date, @plot_name, species = if result.length == 3
-          [result[0], result[1], result[2]]
-        elsif result.length == 4
-          [result[0], result[1] + '.' + result[2], result[3]]
-        end
+                                    [result[0], result[1], result[2]]
+                                  elsif result.length == 4
+                                    [result[0], "#{result[1]}.#{result[2]}", result[3]]
+                                  end
 
-      return unless @plot_name.start_with?('T')
+      # return unless @plot_name.start_with?('T')
 
       year = date[0..3].to_i
       month = date[4..5].to_i
@@ -27,16 +28,26 @@ module Parsers
       fraction = nil
       # drop the last letter of the species usually A, B, C if it is present
       species = species.slice(0, species.length - 1) if species.end_with?('A', 'B', 'C')
-      if species.include?('.')
-        species, fraction = species.split('.')
-      end
-      @plot_name = @plot_name.gsub(/0(\d)/,'\1')
-      @plot_name = @plot_name + '-' + species + (fraction ? '.' + fraction : '')
+      species, fraction = species.split('.') if species.include?('.')
+      @plot_name = @plot_name.gsub(/0(\d)/, '\1')
+      study = if @plot_name.start_with?('T')
+                Study.find_by(name: 'LTER')
+              elsif @plot_name.start_with?('G')
+                Study.find_by(name: 'GLBRC')
+              elsif @plot_name.start_with?('ASP')
+                Study.find_by(name: 'LTAR')
+              elsif @plot_name.start_with?('BAF')
+                Study.find_by(name: 'LTAR')
+              else
+                raise "Unknown study for plot #{@plot_name}"
+              end
+      fraction_string = fraction ? ".#{fraction}" : ''
+      @plot_name = "#{@plot_name}-#{species}#{fraction_string}"
 
-      plot = Plot.find_or_create_by(name: @plot_name, study_id: 1)
-      plot.species_code = species
-      plot.fraction = fraction
-      plot.save
+      # plot = Plot.find_or_create_by(name: @plot_name, study_id: 1)
+      plot = Plot.find_or_create_by(name: @plot_name, study_id: study.id)
+      return unless plot
+
       process_data
     end
   end
--- spec/lib/parsers/cn_unicube_parser_spec.rb
+++ spec/lib/parsers/cn_unicube_parser_spec.rb
@@ -1,15 +1,16 @@
 # frozen_string_literal: true
 
-require "rails_helper"
+require 'rails_helper'
 
 describe Parsers::CNUnicubeParser do
   describe 'a line of data' do
     before do
+      find_or_factory :study, name: 'LTER'
       @parser = Parsers::Parser.for(51, Date.today)
       @parser.process_line('4.78,20201019-T01R2-ZEAMX.GR,Plant,2752,59724,0.9989,1.0996,1.2967,43.3262,33.4116,,0,0,0,0,,2025-02-26,14:56:45')
     end
     it 'should have the right date' do
-      expect(@parser.sample.sample_date).to eq(Date.civil(2020,10,19))
+      expect(@parser.sample.sample_date).to eq(Date.civil(2020, 10, 19))
     end
 
     it 'should have the right plot' do
@@ -17,12 +18,11 @@ describe Parsers::CNUnicubeParser do
     end
 
     it 'should have the right carbon measurement' do
-      expect(@parser.sample.measurements.collect {|x| x.amount}).to include(1.2967)
+      expect(@parser.sample.measurements.collect { |x| x.amount }).to include(1.2967)
     end
 
     it 'should have the right nitrogen measurement' do
-      expect(@parser.sample.measurements.collect {|x| x.amount}).to include(43.3262)
+      expect(@parser.sample.measurements.collect { |x| x.amount }).to include(43.3262)
     end
   end
-
 end
